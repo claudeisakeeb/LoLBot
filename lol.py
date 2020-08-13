de253e8ed8b21df59c690e1981a2be875853e9ad
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2020-08-12 19:16:07 -0700

    blurred api

--- lol.py
+++ lol.py
@@ -1,7 +1,7 @@
 import requests
 
 def getSummonerInfo(region, name):
-    url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key=RGAPI-8ee22b71-1933-4ba1-8c17-cf8c92ca2324"
+    url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key=XXX"
     response = requests.get(url)
     print(response.json())
 
