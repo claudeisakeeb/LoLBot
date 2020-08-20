5d5bd67e58c4b6bb2827a03ccb077855b4b9c954
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2020-08-19 19:19:08 -0700

    fixed url encoding

--- youtube.py
+++ youtube.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import discord
+import urllib.parse
 
 with open("token.txt", "r") as f:
     sample_text1 = f.readline()
@@ -8,23 +9,19 @@ with open("token.txt", "r") as f:
     YOUTUBE_API_KEY = f.readline()
 
 def getYoutubeVideos(args):
-
-    def generateQuery(args):
-        return  "%20".join(args)
-
     args = list(args)
     maxResults = 3
     if args and args[-1].isdigit() and 1 <= int(args[-1]) <= 5:
         maxResults = args[-1]
         args = args[:len(args)-1]
-    url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={generateQuery(args)}&maxResults={maxResults}&type=video&key={YOUTUBE_API_KEY}"
+    url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={urllib.parse.quote(' '.join(args))}&maxResults={maxResults}&type=video&key={YOUTUBE_API_KEY}"
+    print(url)
     data = requests.get(url).json()["items"]
     embed = discord.Embed(
         title=f"Top {maxResults} results for '{' '.join(args)}':",
         colour=discord.Colour.red()
     )
     for item in data:
-        print(item["id"])
         description = f"https://youtube.com/watch?v={item['id']['videoId']}\n"
         to_add = item["snippet"]["description"]
         description += f"{to_add[:min(len(to_add), 80)]}..."
@@ -33,5 +30,6 @@ def getYoutubeVideos(args):
         embed.set_image(url=data[0]["snippet"]["thumbnails"]["high"]["url"])
     return embed
 
+getYoutubeVideos(["tom&jerry"])
 #request = client.search().list()
 #repsonse = request.execute()
