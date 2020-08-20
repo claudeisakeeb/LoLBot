4a739ebf0ff3acf40ff6bb74fb68f20e4c181b9f
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2020-08-19 19:22:35 -0700

    added youtube feature

--- youtube.py
+++ youtube.py
@@ -15,7 +15,6 @@ def getYoutubeVideos(args):
         maxResults = args[-1]
         args = args[:len(args)-1]
     url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={urllib.parse.quote(' '.join(args))}&maxResults={maxResults}&type=video&key={YOUTUBE_API_KEY}"
-    print(url)
     data = requests.get(url).json()["items"]
     embed = discord.Embed(
         title=f"Top {maxResults} results for '{' '.join(args)}':",
@@ -29,7 +28,5 @@ def getYoutubeVideos(args):
     if len(data) > 0:
         embed.set_image(url=data[0]["snippet"]["thumbnails"]["high"]["url"])
     return embed
-
-getYoutubeVideos(["tom&jerry"])
 #request = client.search().list()
 #repsonse = request.execute()
